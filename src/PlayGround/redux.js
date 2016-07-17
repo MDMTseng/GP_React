f7d5414483e1ff91c835a25a49f0b0134432d778
MDM Tseng <chiapintseng@gmail.com>
2016-07-16 23:15:30 -0700

    redux playaround

--- src/PlayGround/redux.js
+++ src/PlayGround/redux.js
@@ -1,35 +1,78 @@
-import { createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 
-const reducer = (state, action) => {
+const calcReducer = (state = {ans:0}, action) => {
 
+  switch(action.type)
+  {
+    case "ADD":
+      state.ans += action.data;
+    break;
+    case "SUB":
+      state.ans -= action.data;
+    break;
+    case "MUL":
+      state.ans *= action.data;
+    break;
+    case "DIV":
+      state.ans /= action.data;
+    break;
+    case "SET":
+      state.ans  = action.data;
+    break;
+  }
+  return state;
+}
+const userReducer = (state = {}, action) => {
 
-  if (action.type === "ADD") {
-    state.count += action.data;
+  if (action.type === "USER") {
+    state.ID=action.data.ID;
+    state.Name=action.data.Name;
     return state;
-  }else if (action.type === "SUB") {
-    state.count -= action.data;
-    return state;
-  }else if (action.type === "MUL") {
-    state.count *= action.data;
-    return state;
-  }else if (action.type === "DIV") {
-    state.count /= action.data;
-    return state;
-  }else if (action.type === "SET") {
-    state.count = action.data;
-    return state;
+  }
+  if(action.type === "ERROR")
+  {
+    throw new Error("ERROR act");
   }
   return state;
 }
 
-const store = createStore(reducer, {count:0})
-
-store.subscribe(() => {
-  console.log("store changed", store.getState());
+const reducer_C = combineReducers({
+  calcData:calcReducer,
+  userData:userReducer,
 })
+
+
+const logger = (store) => (next) => (action) =>{
+  console.log("store :", store);
+  console.log("action fired", action);
+  next(action)
+}
+
+const error_catch = (store) => (next) => (action) =>{
+  try{
+    next(action)
+  }catch(e)
+  {
+    console.log("ERROR::",e);
+  }
+
+}
+
+const middleware = applyMiddleware(logger,error_catch);
+
+const store = createStore(reducer_C,{},middleware)
+
 
 store.dispatch({type: "SET",data:10000})
 store.dispatch({type: "ADD",data:1})
 store.dispatch({type: "SUB",data:2})
 store.dispatch({type: "MUL",data:3})
 store.dispatch({type: "DIV",data:4})
+store.dispatch({type: "USER",data:
+  {
+    ID:Date.now() ,
+    Name:"MDM"
+  }
+})
+
+store.dispatch({type: "ERROR",data:{}})
